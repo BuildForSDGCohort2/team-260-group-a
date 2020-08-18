3d9078d171591869a49564327b5f684b19d821fd
Werner Barnard <wbarnard81@gmail.com>
2020-08-18 13:35:59 +0200

    Created the basic routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 require("dotenv").config();
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+
+//Routes
+const users = require("./routes/users");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/users", users);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on Port: ${process.env.PORT}`);
